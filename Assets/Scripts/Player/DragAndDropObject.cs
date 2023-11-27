4e3429c14e21f7782f3321383b65983de2a32716
Powertr1p <sc2aru@gmail.com>
2023-11-27 20:14:39 +0400

    added logic to determine is player dragged object or simply clicked

--- Assets/Scripts/Player/DragAndDropObject.cs
+++ Assets/Scripts/Player/DragAndDropObject.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 public class DragAndDropObject : MonoBehaviour
 {
    private Camera _camera;
    private Vector3 _mousePosition;
+   private Vector3 _lastMouseUpPosition;
+   private Vector3 _lastMouseDownPosition;
+
+   private float _delta;
+   private bool _isDrag;
+   private bool _isDown;
 
    private void Awake()
    {
@@ -17,11 +24,28 @@ public class DragAndDropObject : MonoBehaviour
 
    private void OnMouseDown()
    {
+      _isDown = true;
       _mousePosition = Input.mousePosition - GetMousePosition();
+      _lastMouseDownPosition = Input.mousePosition;
+   }
+
+   private void OnMouseUp()
+   {
+      _lastMouseUpPosition = Input.mousePosition;
+
+      if (_isDown && _lastMouseUpPosition == _lastMouseDownPosition)
+      {
+         Debug.Log("SHOW CARD");
+      }
+      
+      _isDrag = false;
+      _isDown = false;
    }
 
    private void OnMouseDrag()
    {
+      _isDrag = true;
+
       transform.position = _camera.ScreenToWorldPoint(Input.mousePosition - _mousePosition);
    }
 }
