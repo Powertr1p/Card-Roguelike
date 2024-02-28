2f9097c80985815e05928d09206a66e653fda245
Powertr1p <sc2aru@gmail.com>
2024-02-28 18:58:52 +0400

    fixed a bit logic

--- Assets/Scripts/Player/KeyboardInput.cs
+++ Assets/Scripts/Player/KeyboardInput.cs
@@ -28,41 +28,50 @@ namespace Player
         {
             if (!_canReceiveInput) return;
             _canReceiveInput = false;
-            
-            if (GameStateGetter.State == TurnState.PlayerPositioningTurn && horizontalInput != 0)
-            {
-                HandlePositioningStateMovement(horizontalInput);
-                _canReceiveInput = true;
-                return;
-            }
 
+            if (GameStateGetter.State == TurnState.PlayerPositioningTurn)
+            {
+                if (horizontalInput != 0 && verticalInput == 0)
+                {
+                    HandlePositioningStateMovement(horizontalInput);
+                    _canReceiveInput = true;
+                    return;
+                }
+            }
+            
             var desirePosition = GetTargetCardPosition(horizontalInput, verticalInput);
             var target = _raycaster.GetTarget(desirePosition);
             
             _player.StartDragState();
+            _player.transform.position = desirePosition;
             _player.KeyboardDrag(target);
             _canReceiveInput = true;
         }
 
         private Vector3 GetTargetCardPosition(float horizontalInput, float verticalInput)
         {
-            var movementOffset =
-                new Vector3(_deckSpawner.Offset.x * horizontalInput, _deckSpawner.Offset.y * verticalInput, 0f);
-            var desirePosition = _player.transform.position + movementOffset;
+            Vector3 movementOffset = new Vector3(_deckSpawner.Offset.x * horizontalInput, _deckSpawner.Offset.y * verticalInput, 0f);
+            Vector3 desirePosition = _player.transform.position + movementOffset;
+            
             return desirePosition;
         }
 
         private void HandlePositioningStateMovement(float horizontalInput)
         {
-            var nextPosition = _player.transform.position + new Vector3(_deckSpawner.Offset.x * horizontalInput, 0f, 0f);
-
-            var hit = _raycaster.GetTarget(_player.transform.position +
-                                           new Vector3(_deckSpawner.Offset.x * horizontalInput, _deckSpawner.Offset.y, 0f));
-
-            if (!ReferenceEquals(hit.collider, null) && hit.collider.TryGetComponent(out Placement placement))
+            Vector3 playerPosition = _player.transform.position;
+            Vector3 nextPosition = playerPosition + new Vector3(_deckSpawner.Offset.x * horizontalInput, 0f, 0f);
+            
+            if (IsPlacementAbovePosition(nextPosition))
             {
                 _player.transform.position = nextPosition;
             }
+        }
+
+        private bool IsPlacementAbovePosition(Vector3 position)
+        {
+            var hit = _raycaster.GetTarget(position + new Vector3(0f, _deckSpawner.Offset.y, 0f));
+
+            return !ReferenceEquals(hit.collider, null) && hit.collider.TryGetComponent(out Placement placement);
         }
     }
 }
